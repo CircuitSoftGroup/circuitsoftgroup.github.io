23c953570f41c26cab147cf54e5e7926e40f0269
mastercoms <circuitsoft@openmailbox.org>
2015-05-12 23:06:00 -0400

    fix glitching

--- home.js
+++ home.js
@@ -1,6 +1,6 @@
 // Animation fix
-timeoutMarginCorrection = window.setTimeout(setCorrectMargin, 3000);
-function setCorrectMargin() {
+timeoutMarginCorrection = window.setTimeout(setCorrectStyles, 3000);
+function setCorrectStyles() {
   $("#main-intro").css("-webkit-animation", "1s unmargin");
   $("#main-intro").css("animation", "1s unmargin");
   $("#secondary-intro").css("-webkit-animation", "1s unmargin");
@@ -9,8 +9,5 @@ function setCorrectMargin() {
   $("#secondary-intro").css("top", "0");
   $("#main-intro").css("transform", "translate(0, 0)");
   $("#secondary-intro").css("transform", "translate(0, 0)");
-}
-timeoutHeightCorrection = window.setTimeout(setCorrectHeight, 3500);
-function setCorrectHeight() {
-	$("#intro-jumbotron").css("height", "40vh");
+  $("#intro-jumbotron").css("height", "40vh");
 }
