8a88127327bc9d7da69bf438491e150ca432f72b
mastercoms <circuitsoft@openmailbox.org>
2015-05-15 02:54:07 -0400

    Adjust timings back from async
    Now that we don't use async JS, we don't have to adjust the times.

--- home.js
+++ home.js
@@ -1,5 +1,5 @@
 // Animation fix
-timeoutMarginCorrection = window.setTimeout(setCorrectStyles, 3000);
+timeoutMarginCorrection = window.setTimeout(setCorrectMargins, 3000);
 function setCorrectStyles() {
   $("#main-intro").css("-webkit-animation", "1s unmargin");
   $("#main-intro").css("animation", "1s unmargin");
@@ -9,5 +9,8 @@ function setCorrectStyles() {
   $("#secondary-intro").css("top", "0");
   $("#main-intro").css("transform", "translate(0, 0)");
   $("#secondary-intro").css("transform", "translate(0, 0)");
+}
+timeoutMarginCorrection = window.setTimeout(setCorrectHeight, 4000);
+function setCorrectHeight() {
   $("#intro-jumbotron").css("height", "40vh");
 }
